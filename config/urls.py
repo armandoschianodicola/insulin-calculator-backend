227c35e46a17e07c040648c769d1c534295b4552
Armando Schiano di Cola <armando.schianodicola@gmail.com>
2023-06-03 12:16:42 +0200

    upd

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from food import urls as food_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('food/', include(food_urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
